6345f95f5c0857db716b8e0544274171c74eb657
Sharon131 <pastula.magdalena@gmail.com>
2020-01-20 18:39:10 +0100

    Initial commit.

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Problem - wyszukiwanie skojarzenia pełnego w grafie.
+// Ogólnie algorytm Edmondsa.
+// Można też zastosować algorytm max. przepływu Edmondsa-Karpa np.
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
